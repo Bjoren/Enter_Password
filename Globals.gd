1bb367fcd3783806c4f0e72d17abdcdca7443b14
Bjoren <Bjoren92@gmail.com>
2021-08-13 23:58:36 +0200

    global script

---
+++ Globals.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var player_position = Vector2.ZERO
+
+func get_player_position():
+	return player_position
+	
+func set_player_position(new_position):
+	player_position = new_position
