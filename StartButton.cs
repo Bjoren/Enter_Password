7baae7a1fd717b86eaa3a90963c714b3ff7a6e95
Sebastian Lindqvist <sebastian.lindqvist@lysa.se>
2021-08-14 17:53:20 +0200

    Added retry button

---
+++ StartButton.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class StartButton : Godot.Button
+{
+	Menu menuNode;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		menuNode = this.GetNode<Menu>("../../Menu");
+	}
+
+	public override void _Pressed()
+	{
+		Node globals = this.GetNode<Node>("/root/Globals");
+		globals.Call("set_player_is_alive", true);
+		menuNode.HasBeenAlive = true;
+		this.Hide();
+		base._Pressed();
+	}
+
+	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+	//  public override void _Process(float delta)
+	//  {
+	//      
+	//  }
+}
