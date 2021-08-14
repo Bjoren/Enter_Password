bcc51f5f248d7618fdb7d4220eb28f44ec24aff7
Bjoren <Bjoren92@gmail.com>
2021-08-15 00:49:05 +0200

    fix errors

--- EncryptedFolder.gd
+++ EncryptedFolder.gd
@@ -1,15 +1,18 @@
 extends KinematicBody2D
 
-var health = 40
+var max_health = 100.0
+var health = max_health
 
 func _ready():
-	position = Vector2(100 + randi() % 1720, 200 + randi() % 880)
+	position = Vector2(1920/2, 1080/2)
 
-func _physics_process(delta):
-	pass
+#func _physics_process(delta):
+#	pass
 	
 func hurt():
 	health -= 1
+	
+	$Percent.text = str(int(health/max_health*100)) + "%"
 	
 	if health == 0:
 		print("Win")
