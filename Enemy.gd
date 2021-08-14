0985dc6002760fd5241a76455865e376c06de4e0
Bjoren <Bjoren92@gmail.com>
2021-08-14 11:52:54 +0200

    Enemy movement updated

--- Enemy.gd
+++ Enemy.gd
@@ -1,7 +1,17 @@
 extends KinematicBody2D
 
-func _ready():
-	pass # Replace with function body.
+export var acceleration:int = 600
+export var turn_speed:float = 0.03
 
-#func _process(delta):
-#	pass
+var velocity:Vector2 = Vector2.ZERO
+
+func _ready():
+	pass # Självaste hjärtat i modermodemet
+
+func _physics_process(_delta):
+	var player_position = Globals.get_player_position()
+	var new_velocity = Vector2(-acceleration, 0).rotated(global_position.angle_to_point(player_position))
+	
+	velocity = lerp(velocity, new_velocity, turn_speed)
+	move_and_slide(velocity)
+	look_at(Globals.get_player_position())
