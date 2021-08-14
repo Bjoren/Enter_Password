dd195633033225ebab1ec0ebd90455a90349cfa9
Bjoren <Bjoren92@gmail.com>
2021-08-14 11:26:56 +0200

    enmy :-))

--- Globals.gd
+++ Globals.gd
@@ -1,9 +1,13 @@
 extends Node
 
-var player_position = Vector2.ZERO
+var player_position:Vector2 = Vector2.ZERO
+var current_difficulty:int = 1
 
 func get_player_position():
 	return player_position
 	
 func set_player_position(new_position):
 	player_position = new_position
+
+func get_current_difficulty():
+	return current_difficulty
