ad2e78304149067ccbd2b877cd094dc50ce5fe06
Sebastian Lindqvist <sebastian.lindqvist@lysa.se>
2021-08-15 12:55:20 +0200

    Level text size decrease and increased difficulty

--- TwinStick.gd
+++ TwinStick.gd
@@ -23,7 +23,7 @@ func _physics_process(_delta):
 			waves += 1
 			if waves % 2 == 0 && Globals.get_current_level() < 3:
 				spawn_hint()
-			if waves % 8 == 0:
+			if waves % 5 == 0:
 				Globals.increase_current_difficulty()
 			
 			$Spawn_timer.start()
